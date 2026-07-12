8889a36a69205e4fa31b5b78076744768fee3c35
Huzefa <huzefa@gmail.com>
2026-07-12 02:54:47 +0000

    basic pyhon code added

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+# ─────────────────────────────────────────────
+# MY FIRST AI CHATBOT
+# Built at HuzefAI Seminar
+# ─────────────────────────────────────────────
+
+# Step 1: Course information
+courses = {
+    "AWS":    "Rs.4999 — 6 weeks — Cloud computing",
+    "DevOps": "Rs.7999 — 8 weeks — Build and deploy apps",
+    "GenAI":  "Rs.9999 — 8 weeks — Build AI applications",
+    "Python": "Rs.3999 — 5 weeks — Programming basics"
+}
+
